5999917b39caafaba5d0b10169e7e06c4cd1096e
morrowwm <morrowwm@yahoo.ca>
2025-01-30 20:22:19 -0400

    image and overlay displaying
    Navigation doesn't work yet though

--- aligner.php
+++ aligner.php
@@ -5,28 +5,23 @@
 		<script src="https://code.jquery.com/jquery-3.1.1.min.js"></script> 
 		<style>
 			.image {
-				position: relative;
-				top: 0;
-				left: 0;
-			}
-			.live {
-				position: relative;
-				top: 100;
-				left: 100;
-				border: 1px solid #000000;
+				position: absolute;
+				top: 40px;
+				left: 40px;
+				//border: 1px solid #000000;
 				z-index:1
 			}
 			.starmap {
 				position: absolute;
-				top: -1200px;
-				left: -300px;
+				top: 40px;
+				left: 40px;
 				border: 1px solid #000000;
 				z-index:5
 			}
 			.navigate {
 				position:absolute; 
-				left: 0;
-				display:inline-block;
+				top: 0px
+				left: 200px;
 				z-index:10
 			}
 		</style>
@@ -35,72 +30,119 @@
 		<script src="virtualsky/stuquery.js"></script>
 		<script src="virtualsky/virtualsky.js"></script>
 		<div class="container" id="working-area" style="width:1000px;"> 
-				<h3>Constellation Aligner</h3>  
 			<div class="navigate">
 				<form id="form-file_chooser" class="form-file_chooser" action="" method="post" autocomplete="off">
-					<label for="form-imgfile">Image </label>
-					<input type="text" id="form-imgfilename" name="form-imgfile" required autofocus value="mages/20250124/image-20250125021912.jpg" size="48">
+					<label for="form-imgfilename">Image</label>
+					<input type="text" id="form-imgfilename" name="form-imgfile" required autofocus value="mages/20250124/image-20250125021912.jpg" size="32">
 					<input type="submit" id="loadimage" value="Load">
+					<button type="button" id="cw" value = "CW">&#8635;</button>
+					<button type="button" id="ccw" value = "CCW">&#8634;</button>
+					<button type="button" id="up" value = "U">&#11014;</button>
+					<button type="button" id="down" value = "D">&#11015;</button>
+					<button type="button" id="left" value = "L">&#11013;</button>
+					<button type="button" id="right" value = "R">&#10145;</button>
+					<button type="button" id="bigger" value = "R">&#10133;</button>
+					<button type="button" id="smaller" value = "R">&#10134;</button>
 				</form>
-			
-				<button type="button" id="cw" value = "CW">CW</button>
-				<button type="button" id="ccw" value = "CCW">CCW</button>
-				<button type="button" id="up" value = "U">U</button>
-				<button type="button" id="down" value = "D">D</button>
-				<button type="button" id="left" value = "L">L</button>
-				<button type="button" id="right" value = "R">R</button>
+
 			</div>
 			<script  type="text/javascript">
+				var virtualSkyData = null;
+				
 				$(document).ready(function () {
 					var planetarium;
 					var imagefilename;
+					var configData = "configuration.json"
+					
+					$.ajax({
+						// No need for ?_ts=   since $.ajax adds one
+						url: configData,
+						cache: false,
+						dataType: 'json',
+						error: function(jqXHR, textStatus, errorThrown) {
+							// console.log("jqXHR=", jqXHR);
+							// console.log("textStatus=" + textStatus + ", errorThrown=" + errorThrown);
+							// TODO: Display the message on the screen.
+							if (jqXHR.status == 404) {
+								console.log(configData + " not found!");
+							} else {
+								console.log("Error reading '" + configData + "': " + errorThrown);
+							}
+						},
+						success: function (data) {
+							var c = data.config;
+							virtualSkyData = c;
+							virtualSkyData.width = virtualSkyData.overlayWidth;
+							virtualSkyData.height = virtualSkyData.overlayHeight;
+
+							virtualSkyData.id = 'starmap',	// This should match the ID used in the DOM
+							virtualSkyData.transparent = true,
+							virtualSkyData.color = 'rgb(0,120,120)',
+							virtualSkyData.scalestars = 3,
+							virtualSkyData.showstarlabels = true,
+							virtualSkyData.constellations = true,
+							virtualSkyData.latitude = 44.803842, 
+							virtualSkyData.longitude = -63.62060,
+							virtualSkyData.opacity = 0.5,
+							virtualSkyData.atmosphere = false,
+							virtualSkyData.keyboard = false,
+							virtualSkyData.mouse = false,
+							virtualSkyData.live = false,  // don't move while working on alignment
+							virtualSkyData.clock = new Date("January 05, 2025 02:19:12")
+
+							planetarium = S.virtualsky(virtualSkyData);
+							console.log("Success reading " + configData);
+						}
+					});
 					
 					$(".navigate").on( "click", function (event) {
 						event.preventDefault(); // Prevent default form submission
 						event.stopPropagation();
 						var navElement = event.target.id;
 						console.log("input clicked", navElement, " value: ", navElement);
-						if(navElement === "up"){
+						if(navElement === "up") {
 							console.log("y translate");
+							virtualSkyData.overlayOffsetTop = virtualSkyData.overlayOffsetTop - 10;
 						}
-						if(navElement === "loadimage"){
+						else if(navElement === "down") {}
+						else if(navElement === "left") {}
+						else if(navElement === "right") {}
+						else if(navElement === "cw") {
+							virtualSkyData.az = virtualSkyData.az + 0.1;
+						}
+						else if(navElement === "ccw") {
+							virtualSkyData.az = virtualSkyData.az - 0.1;
+						}
+						else if(navElement === "bigger") {
+							virtualSkyData.width = virtualSkyData.width + 50;
+						}
+						else if(navElement === "smaller") {
+							virtualSkyData.width = virtualSkyData.width - 50;
+						}
+						
+						else if(navElement === "loadimage") {
 							var imageSrc = $( "#form-imgfilename" ).val();
+							
 							
 							console.log("Attempting load of: " + imageSrc);
 							$("#working-image").attr("src", imageSrc)
 								.attr("height", 760)
 								.attr("width", 1014);
+							
 						}
+
+						planetarium = S.virtualsky({virtualSkyData});
 					});
-					
-					planetarium = S.virtualsky({
-						id: 'starmap',	// This should match the ID used in the DOM
-						transparent: true,
-						color:'rgb(0,120,120)',
-						scalestars: 5,
-						projection: 'allsky_aligner',
-						showstarlabels: true,
-						constellations: true,
-						latitude: 44.803842, 
-						longitude: -63.62060,
-						clock: new Date("January 05, 2025 02:19:12")
-					});
-					$("#starmap")
-					.css("margin-top", -1000 + "px")
-					.css("margin-left", -1000 + "px");
 				});
 			</script>
 			
-			<div id="imageContainer" class="image" style="max-width: 2000px">
-				<div id="live_container" class="live">
-					<img title="allsky image" alt="allsky image" id="working-image" class="working-image" src="#"  alt="image" style="margin-right: -100%">
-				</div>
-				<div id="starmap_container" class="starmap">
-					<div id="starmap" style="width:3300px;height:3200px"></div>
-				</div>
+			<div id="image_container" class="image">
+				<img title="allsky image" alt="allsky image" id="working-image" class="working-image" src="#" alt="image">
 			</div>
-			
-		</div>  
+			<div id="starmap_container" class="starmap">
+				<div id="starmap" style="width:3000px;height:3000px"></div>
+			</div>			
+		</div>
 	</body>  
 </html>  
 
